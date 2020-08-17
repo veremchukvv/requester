6b2bab270390ddaa5324c1f0a506e3ce31cdbaac
slon <veremchukvv@gmail.com>
2020-08-17 18:58:42 +0300

    +

--- requester.go
+++ requester.go
@@ -1,5 +1,36 @@
 package requester
 
-func main {
-	
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func patchJSON(url string, params url.Values, result interface{}) error {
+	request, err := http.NewRequest(
+		http.MethodPatch,
+		url,
+		strings.NewReader(params.Encode()),
+	)
+	if err != nil {
+		return fmt.Errorf("Response error: %w", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Bad status: %d", response.StatusCode)
+	}
+	if result == nil {
+		return nil
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("Can't read response %w", err)
+	}
+	err = json.Unmarshal(body, result)
+	if err != nil {
+		return fmt.Errorf("Can't parse response %w", err)
+	}
+	return nil
 }
