c61980c9567220877c6f0aebeee7b6036380fa7c
slon <veremchukvv@gmail.com>
2020-08-18 18:06:07 +0300

    fix requester

--- requester.go
+++ requester.go
@@ -25,19 +25,39 @@ func PatchJSON(url string, params url.Values, result interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Response error: %w", err)
 	}
+
 	if response.StatusCode != http.StatusOK {
 		return fmt.Errorf("Bad status: %d", response.StatusCode)
 	}
+
 	if result == nil {
 		return nil
 	}
+
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return fmt.Errorf("Can't read response %w", err)
 	}
+
 	err = json.Unmarshal(body, result)
 	if err != nil {
 		return fmt.Errorf("Can't parse response %w", err)
+	}
+	return nil
+}
+
+func GetJSON(url string, out interface{}) error {
+	res, err := http.DefaultClient.Get(url)
+	if err != nil {
+		return fmt.Errorf("make request error: %w", err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("read response error: %w", err)
+	}
+	err = json.Unmarshal(body, out)
+	if err != nil {
+		return fmt.Errorf("parse response error '%s': %w", body, err)
 	}
 	return nil
 }
